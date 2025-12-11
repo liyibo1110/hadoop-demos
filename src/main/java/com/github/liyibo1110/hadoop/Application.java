62b9d0629cf09d603a1887550b1bee539c4c5520
liyibo1110 <liyibo1110@sina.com>
2025-12-11 18:10:49 +0800

    初次提交

---
+++ src/main/java/com/github/liyibo1110/hadoop/Application.java
@@ -0,0 +1,13 @@
+package com.github.liyibo1110.hadoop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
